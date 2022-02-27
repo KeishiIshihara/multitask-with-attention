e79132d5959808a86dea2723b6be987595cb05f3
keishi <ishihara@aisl.cs.tut.ac.jp>
2022-02-27 21:48:54 +0900

    Mod download script

--- download_weights.sh
+++ download_weights.sh
@@ -1,4 +1,4 @@
 fileid="16S0fYXOXhYtuVLo-Ia8x8q5g3lhkrC3R"
-filename="ckpts_.zip"
+filename="ckpts.zip"
 html=`curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}"`
 curl -Lb ./cookie "https://drive.google.com/uc?export=download&`echo ${html}|grep -Po '(confirm=[a-zA-Z0-9\-_]+)'`&id=${fileid}" -o ${filename}
