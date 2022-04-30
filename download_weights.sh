478e6a2947ea7e886b9e6cf7c96be6e954f68e3a
keishi <ishihara@aisl.cs.tut.ac.jp>
2022-04-30 22:10:09 +0900

    [Update] Updated checkpoints download script

--- download_weights.sh
+++ download_weights.sh
@@ -1,4 +1,6 @@
-fileid="16S0fYXOXhYtuVLo-Ia8x8q5g3lhkrC3R"
+# https://drive.google.com/file/d/1J4GcBwFiL-d8UJwD8OvHffUAJtnFmPYx/view?usp=sharing
+fileid="1J4GcBwFiL-d8UJwD8OvHffUAJtnFmPYx"
 filename="ckpts.zip"
 html=`curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}"`
 curl -Lb ./cookie "https://drive.google.com/uc?export=download&`echo ${html}|grep -Po '(confirm=[a-zA-Z0-9\-_]+)'`&id=${fileid}" -o ${filename}
+unzip ckpts.zip
